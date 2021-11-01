9dbc62193da9580533843f9c08570f7d0c2a60de
Mirafzal0713 <usmonjonovmirafzal0713@gmail.com>
2021-11-01 10:44:45 +0500

    part 1 codes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 
 import './index.css';
 
@@ -102,35 +102,366 @@ import './index.css';
 
 
 
-const Header =  (props) => {
-  return(
-   < p> {props.course}</p>  
-  )
+// const Header =  (props) => {
+//   return(
+//    < p> {props.course}</p>  
+//   )
 
+// }
+// const Content =  (props) => {
+//   return(
+//     < p> {props.info}</p>  
+//   )
+
+//  }
+//  const Total =  (props) => {
+//    return(
+//    < p> {props.average}</p>  
+//    )
+
+//  }
+// const App = () => {
+//   // const-definitions
+// const course = "HTML"
+// const info= " it is the best course"
+// const average = '2000 million'
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content info={info} />
+//       <Total average={average} />
+//     </div>
+//   )
+// }
+// const Header = (props) => {
+//   console.log(props)
+//   return (
+//     <>
+//       {/* <h1>{props.course}</h1> */}
+//       {/* <h2> {props.logo}</h2> */}
+
+
+//     </>
+//   )
+// };
+
+// // const App = () => {
+// //   const course = 'Half Stack application development'
+// //   const part1 = {
+// //     name: 'Fundamentals of React',
+// //     exercises: 10
+// //   }
+// //   const part2 = {
+// //     name: 'Using props to pass data',
+// //     exercises: 7
+// //   }
+// //   const part3 = {
+// //     name: 'State of a component',
+// //     exercises: 14
+// //   }
+
+// //   return (
+// //     <div>
+// //       <Header course={course} part1={  part1.exercises} padrt1={part1.name } part2={ part2} part3={ part3}/>
+// //     </div>
+// //   )
+// // }
+// const Content = (props) => {
+//   const ilk = props.parts
+//   // const il = ilk
+//   console.log(ilk)
+//   return(
+
+//     <>
+//     {/* <h1>{props.parts}</h1> */}
+
+//     </>
+//   )
+// };
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const parts = [
+//     {
+//       name: 'Fundamentals of React',
+//       exercises: 10
+//     },
+//     {
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]
+
+//   // return (
+//   //   // <div>
+//   //   //    <Header course={course} logo={parts[2].name}/>
+//   //   // </div>
+
+//   // )
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content parts={parts} />
+//       {/* <Total parts={parts} /> */}
+//     </div>
+//   )
+// }
+
+
+// const Total= () => {
+//   return(
+//     <>
+
+
+//     </>
+//   )
+// }
+
+
+
+
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+//   const lll = course.name
+//   console.log(lll)
+//   const kkk = course.parts[2].name
+//   console.log(kkk)
+
+//   return (
+//     <div>
+//     {lll}
+//     <br/>
+//     {kkk}
+
+//     </div>
+//   )
+// }
+// const Hello = (props) => {
+//     return (
+//         <div>
+//             <p>
+//                 Hello {props.name}, you are {props.age} years old
+//             </p>
+//         </div>
+//     )
+// }
+
+// const App = () => {
+//     const name = 'Peter'
+//     const age = 10
+
+//     return (
+//         <div>
+//             <h1>Greetings</h1>
+//             <Hello name="Maya" age={26 + 10} />
+//             <Hello name={name} age={age} />
+//         </div>
+//     )
+// }
+
+// const Hello = (props) => {
+//     const bornYear = () => {
+//       const yearNow = new  Date().getFullYear()
+//       console.log(yearNow)
+//       return yearNow - props.age
+//     }
+
+//     return (
+//       <div>
+//         <p>
+//           Hello {props.name}, you are {props.age} years old
+//         </p>
+//         <p>So you were probably born in {bornYear()}</p>
+//       </div>
+//     )
+//   }
+
+// const Hello = () => {
+
+
+
+//   const props = {
+//     name: 'Arto Hellas',
+//     age: 35,
+//   }
+
+
+//   const name = props.name
+//   const age = props.age
+
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>Hello {name}, you are {age} years old</p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+
+
+// props  = {
+//   name: 'Arto Hellas',
+//   age: 35,
+// }
+
+// const Hello = (props) => {
+
+// const { name, age} = props 
+
+
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+
+import React, { useState } from 'react'
+
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const handleClick = () => {
+//     console.log('clicked')
+//   }
+
+//   // console.log(New)
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={() => setCounter(counter + 1)}>
+//         plus
+//       </button>
+//     </div>
+//   )
+// }// 
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={() => setCounter(counter + 1)}>
+//         plus
+//       </button>
+//       <button onClick={() => setCounter(0)}>
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={increaseByOne}>
+//         plus
+//       </button>
+//       <button onClick={setToZero}>
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
+// const Display = (props) => {
+//   return (
+//     <div>{props.counter}</div>
+//   )
+// }
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <button onClick={increaseByOne}>
+//         plus
+//       </button>
+//       <button onClick={setToZero}>
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
+const Display = ({ counter }) => {
+  return (
+    <div>{counter}</div>
+  )
 }
-const Content =  (props) => {
-  return(
-    < p> {props.info}</p>  
-  )
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
- }
- const Total =  (props) => {
-   return(
-   < p> {props.average}</p>  
-   )
-  
- }
 const App = () => {
-  // const-definitions
-const course = "HTML"
-const info= " it is the best course"
-const average = '2000 million'
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <Header course={course} />
-      <Content info={info} />
-      <Total average={average} />
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
-export default App;
+export default App
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// let counter = 1
 
+// const refresh = () => {
+    ReactDOM.render(<App  />,document.getElementById('root'))
+// }
 
-ReactDOM.render(<App />,document.getElementById('root'));
-
+// refresh()
+// counter += 1
+// refresh()
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+//   }, 1005555555555)
